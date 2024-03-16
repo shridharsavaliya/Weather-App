fbeab23dc09c62e1980d1d50b6fa016f33f67c47
shridharsavaliya <162824258+shridharsavaliya@users.noreply.github.com>
2024-03-16 18:01:23 +0000

    home page change

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,11 +6,11 @@ export default class Home extends Component {
     return (
       <div>
         <div className='container mt-5'>
-            <div className='row w-75 mx-auto bg-light p-5'>
+            <div className='row w-100 mx-auto bg-light p-4'>
                 <div className='col-xl-6 align-content-center d-grid'>
                     <h1>Weather & Forecast Application</h1>
-                    <p className='fs-4'>Use Weather Application and get weather information daily and daily Download weather forecast for free and experience</p>
-                    <button className='btn btn-secondary text-light w-50'>Download Now</button>
+                    <p className='fs-5'>Use Weather Application and get weather information daily and daily Download weather forecast for free and experience</p>
+                    <button className='btn btn-secondary text-light w-50 mb-3'>Download Now</button>
                 </div>
                 <div className='col-xl-6'>
                     <img src={img} alt="weather img" className='img-fluid' />
